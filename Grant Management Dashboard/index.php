54fef9a93c8ddde6a7e6b90a7ce6c41d9b4f1a50
Zachary McGee <zachary.mcgee@siu.edu>
2018-12-12 15:57:33 -0600

    Create Crude Login Page

---
+++ Grant Management Dashboard/index.php
@@ -0,0 +1,5 @@
+<form action="auth.php" method="post">
+  <input type="text" name="username" placeholder="Username">
+  <input type="password" name="password" placeholder="Password">
+              <button type="submit" id="login-button">Login</button>
+      </form>
