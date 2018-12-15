3c54768375cc407e0e05f795ef98f59d3d1d7d33
Zachary McGee <zachary.mcgee@siu.edu>
2018-12-14 20:05:33 -0600

    Drag and Drop File

---
+++ Grant Management Dashboard/includes/grants/new-grant.php
@@ -0,0 +1,48 @@
+<head>
+<script type="text/javascript">
+let dragarea = document.getElementById('drag-and-drop');
+let fileInput = document.getElementById('file-upload');
+
+dragarea.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  dragarea.classList.add('dragging');
+});
+
+dragarea.addEventListener('dragleave', () => {
+  dragarea.classList.remove('dragging');
+});
+
+dragarea.addEventListener('drop', (e) => {
+  e.preventDefault();
+  dragarea.classList.remove('dragging');
+  fileInput.files = e.dataTransfer.files;
+  readSingleFile(e);
+  //console.log(e);
+});
+
+</script>
+</head>
+<div class="full-card">
+  <div class="card-title">
+    <div class="card-title-text">
+      <i class="fas fa-plus-circle"></i><span class="parent-link">New Grant</span>
+    </div>
+    <div class="card-title-button">
+    </div>
+  </div>
+
+  <div class="card-body">
+    <div id="drag-and-drop" class="drag-and-drop">
+      <div class="drag-and-drop-text">
+        <p>Drag and Drop File Here</p>
+      </div>
+      <div class="drag-and-drop-text-or">
+        <p>or</p>
+      </div>
+      <label for="file-upload" class="custom-file-upload">
+          Select File
+      </label>
+      <input id="file-upload" type="file"/>
+    </div>
+  </div>
+</div>
--- Grant Management Dashboard/js/Dashboard.js
+++ Grant Management Dashboard/js/Dashboard.js
@@ -1,3 +1,20 @@
+/* ACCORDION MENU */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
 /* EXPAND BUTTON */
 function openFullscreen() {
   var elem = document.documentElement;
@@ -23,3 +40,76 @@ function closeFullscreen() {
     document.msExitFullscreen();
   }
 }
+
+function readSingleFile(e) {
+  // IF WE SELECTED THE FILE FROM BUTTON THEN HANDLE THIS WAY
+  if(e.type == "change"){
+  var file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+  }
+  else // ELSE, IT MUST BE FROM DRAG AND DROP, HANDLE THIS WAY
+  {
+    var file = e.dataTransfer.files[0];
+    if(!file) {
+      return;
+    }
+  }
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var contents = e.target.result;
+    loadExcel(contents);
+  };
+  reader.readAsBinaryString(file);
+}
+
+function loadExcel(contents){
+
+    /* Call XLSX */
+    var workbook = XLSX.read(contents, {
+        type: "binary"
+    });
+
+    /* DO SOMETHING WITH workbook HERE */
+    var first_sheet_name = workbook.SheetNames[0];
+    /* Get worksheet */
+    var worksheet = workbook.Sheets[first_sheet_name];
+    var result = XLSX.utils.sheet_to_json(worksheet, {
+        raw: true
+    });
+    console.log(result[0].Description);
+    console.log(result);
+}
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+/* SIDEBAR MENU BUTTONS */
+
+$("#new-grant").click(function(){
+    $.ajax({url: "includes/grants/new-grant.php", success: function(result){
+        $("#content").html(result);
+        $("#breadcrumbs").html("<p>Home / New Grant</p>");
+        document.getElementById('file-upload')
+        .addEventListener('change', readSingleFile, false);
+    }});
+});
+
+/* FUNCTIONS */
