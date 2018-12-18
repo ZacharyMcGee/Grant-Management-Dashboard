2fd21adbe6f09a04c4b08d1bd0ab078f846b5b43
Zachary McGee <zachary.mcgee@siu.edu>
2018-12-18 16:14:23 -0600

    Update dashboard.php

---
+++ Grant Management Dashboard/includes/dashboard/dashboard.php
@@ -0,0 +1,13 @@
+<div class="full-card">
+  <div class="card-title">
+    <div class="card-title-text">
+      <i class="fas fa-plus-circle" id="tasks"></i><span class="parent-link">Dashboard</span>
+    </div>
+    <div class="card-title-button">
+    </div>
+  </div>
+
+  <div class="card-body">
+    <canvas id="myChart"></canvas>
+  </div>
+</div>
