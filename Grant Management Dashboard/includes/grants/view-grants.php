7ba47ae805277b3c5e8b03a4412c208fa35a622e
Zachary McGee <zachary.mcgee@siu.edu>
2019-01-07 14:39:29 -0600

    Format Last Updated Time and Award

--- Grant Management Dashboard/includes/grants/view-grants.php
+++ Grant Management Dashboard/includes/grants/view-grants.php
@@ -8,14 +8,67 @@ if ( mysqli_connect_errno() ) {
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-$sql = "SELECT id, name, bp, award, agency, jsondata FROM grants WHERE userid=" . $_SESSION['id'];
+$sql = "SELECT id, name, bp, dc_award, agency, jsondata, creation_date FROM grants WHERE userid=" . $_SESSION['id'];
 $result = $con->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     $i = 0;
     while($row = $result->fetch_assoc()) {
-        echo "<a href='#' onClick='openGrant(" . $row["id"] . ")'><div class='full-card-left-border-active'><div class='body'><div class='view-grants-chart'><canvas id='chart" . $i . "'></canvas></div><div class='view-grants-info'><span class='view-grants-title'>" . $row["name"] . "</span></div></div></div></a><script>moneyLeftPieChart('chart" . $i . "','" . $row["award"] . "','" . $row["jsondata"] . "');</script>";
+			  $date = new \DateTime();
+				$date->setTimestamp(strtotime($row['creation_date']));
+				$interval = $date->diff(new \DateTime('now'));
+				$timeInSeconds = $interval->format('%s');
+				$timeInMinutes = $interval->format('%i');
+				$timeInHours = $interval->format('%h');
+				$timeInDays = $interval->format('%d');
+				$timeInMonths = $interval->format('%m');
+				$timeInYears = $interval->format('%y');
+
+
+        if($timeInYears <= 0){
+					if($timeInMonths <= 0){
+						if($timeInDays <= 0){
+							if($timeinHours <= 0){
+								if($timeInMinutes <= 0){
+									if($timeInSeconds <= 0){
+                    $timeSinceUpdate = $interval->format('0 seconds ago');
+									}
+									else
+									{
+										$timeSinceUpdate = $interval->format('%s seconds ago');
+									}
+								}
+								else
+								{
+									$timeSinceUpdate = $interval->format('%m minutes ago');
+								}
+							}
+							else
+							{
+								$timeSinceUpdate = $interval->format('%h hours ago');
+							}
+						}
+						else
+						{
+						$timeSinceUpdate = $interval->format('%d days ago');
+					  }
+					}
+					else
+					{
+						$timeSinceUpdate = $interval->format('%m months ago');
+					}
+				}
+				else
+				{
+					$timeSinceUpdate = $interval->format('%y years ago');
+					break;
+				}
+
+
+
+		    $formattedAward = '$' . number_format($row["dc_award"], 2);
+        echo "<a href='#' onClick='openGrant(" . $row["id"] . ")'><div class='full-card-left-border-active'><div class='view-grants-chart'><canvas id='chart" . $i . "'></canvas></div><div class='view-grants-info'><span class='view-grants-title'>" . $row["name"] . " </span><div class='view-grants-bp'> #" . $row["bp"] . "</div><br><span class='view-grants-update'>" . $timeSinceUpdate . "</span><br><span class='view-grants-award'>DC Award: " . $formattedAward . "</span></div><div class='view-grants-buttons'><button class='button'>Manage</button><button class='button'>Report</button><button class='button'>Delete</button></div></div></a><script>moneyLeftPieChart('chart" . $i . "','" . $row["dc_award"] . "','" . $row["jsondata"] . "');</script>";
         $i++;
     }
 }
