a1537e0bbdc45db89e5e6da2ffb5a5af19530b8c
Zachary McGee <zachary.mcgee@siu.edu>
2019-01-11 20:55:07 -0600

    Update view-grants.php

--- Grant Management Dashboard/includes/grants/view-grants.php
+++ Grant Management Dashboard/includes/grants/view-grants.php
@@ -15,6 +15,8 @@ if ($result->num_rows > 0) {
     // output data of each row
     $i = 0;
     while($row = $result->fetch_assoc()) {
+			  // Show when the last we updated the data
+				// We show the biggest value, e.g. if it's a day then we will hide hours and seconds
 			  $date = new \DateTime();
 				$date->setTimestamp(strtotime($row['creation_date']));
 				$interval = $date->diff(new \DateTime('now'));
@@ -25,13 +27,12 @@ if ($result->num_rows > 0) {
 				$timeInMonths = $interval->format('%m');
 				$timeInYears = $interval->format('%y');
 
-
-        if($timeInYears <= 0){
-					if($timeInMonths <= 0){
-						if($timeInDays <= 0){
-							if($timeinHours <= 0){
-								if($timeInMinutes <= 0){
-									if($timeInSeconds <= 0){
+        if($timeInYears <= 0) {
+					if($timeInMonths <= 0) {
+						if($timeInDays <= 0) {
+							if($timeInHours <= 0) {
+								if($timeInMinutes <= 0) {
+									if($timeInSeconds <= 0) {
                     $timeSinceUpdate = $interval->format('0 seconds ago');
 									}
 									else
@@ -62,13 +63,11 @@ if ($result->num_rows > 0) {
 				else
 				{
 					$timeSinceUpdate = $interval->format('%y years ago');
-					break;
 				}
 
-
-
 		    $formattedAward = '$' . number_format($row["dc_award"], 2);
-        echo "<a href='#' onClick='openGrant(" . $row["id"] . ")'><div class='full-card-left-border-active'><div class='view-grants-chart'><canvas id='chart" . $i . "'></canvas></div><div class='view-grants-info'><span class='view-grants-title'>" . $row["name"] . " </span><div class='view-grants-bp'> #" . $row["bp"] . "</div><br><span class='view-grants-update'>" . $timeSinceUpdate . "</span><br><span class='view-grants-award'>DC Award: " . $formattedAward . "</span></div><div class='view-grants-buttons'><button class='button'>Manage</button><button class='button'>Report</button><button class='button'>Delete</button></div></div></a><script>moneyLeftPieChart('chart" . $i . "','" . $row["dc_award"] . "','" . $row["jsondata"] . "');</script>";
+
+        echo "<a href='#' onClick='openGrant(" . $row["id"] . ")'><div class='full-card-left-border-active'><div class='view-grants-chart'><canvas id='chart" . $i . "'></canvas></div><div class='view-grants-info'><span class='view-grants-title'>" . $row["name"] . " </span><div class='view-grants-bp'> #" . $row["bp"] . "</div><br><span class='view-grants-update'> Updated " . $timeSinceUpdate . "</span><br><span class='view-grants-award'>DC Award: " . $formattedAward . "</span></div><div class='view-grants-buttons'><button class='button'>Manage</button><button class='button'>Report</button><button class='button'>Delete</button></div></div></a><script>moneyLeftPieChart('chart" . $i . "','" . $row["dc_award"] . "','" . $row["jsondata"] . "');</script>";
         $i++;
     }
 }
