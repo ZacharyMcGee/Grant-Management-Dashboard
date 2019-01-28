84737cff29d4638f51c781788003d5a1a2fd1dd9
Zachary McGee <zachary.mcgee@siu.edu>
2019-01-27 20:10:06 -0600

    Restructure and Comment

--- Grant Management Dashboard/functions/update-grant.php
+++ Grant Management Dashboard/functions/update-grant.php
@@ -2,42 +2,50 @@
 session_start();
 require_once '../config.php';
 
+// Connect to DB
 $con = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ( mysqli_connect_errno() ) {
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
+
 // Get current user
 $userId = $_SESSION['id'];
+// Get current grant
 $currentGrant = $_SESSION['current_grant'];
-
 // Get POST values
 $jsondata = json_encode($_POST['jsondata']);
 
-$sql2 = "SELECT jsondata, creation_date FROM grants WHERE id=" . $currentGrant;
-$result = $con->query($sql2);
+// First query will get the current grant information
+$sql_getcurrentinfo = "SELECT jsondata, creation_date FROM grants WHERE id=" . $currentGrant;
+$result = $con->query($sql_getcurrentinfo);
 
 if ($result->num_rows > 0) {
   $row = $result->fetch_assoc();
   $old_json = $row['jsondata'];
   $old_date = $row['creation_date'];
 
-  $sql3 = "INSERT INTO grant_archive (grantid, jsondata, creation_date)
+  // Second query will take the current grant info and create an archive
+  $sql_createarchive = "INSERT INTO grant_archive (grantid, jsondata, creation_date)
   VALUES ($currentGrant, '$old_json', '$old_date')";
 
-  if ($con->query($sql3) === TRUE) {
-      echo "Successfuly updated grant: " . $_SESSION['current_grant'];
-  } else {
+  if ($con->query($sql_createarchive) === TRUE) {
+    // Get the new data and insert into the current grant
+    $sql_updatecurrentdata = "UPDATE grants SET jsondata='$jsondata' WHERE id=" . $_SESSION['current_grant'];
+    $sql_updatecreationdate = "UPDATE grants SET creation_date=now() WHERE id=" . $_SESSION['current_grant'];
+
+    if ($con->query($sql_updatecurrentdata) === TRUE && $con->query($sql_updatecreationdate) === TRUE)
+    {
+        echo "Successfuly updated grant!";
+    }
+    else
+    {
+        echo "Error updating record: " . $con->error;
+    }
+  }
+  else 
+  {
       echo "Error updating record: " . $con->error;
   }
-}
-
-$sql = "UPDATE grants SET jsondata='$jsondata' WHERE id=" . $_SESSION['current_grant'];
-$sql4 = "UPDATE grants SET creation_date=now() WHERE id=" . $_SESSION['current_grant'];
-if ($con->query($sql) === TRUE) {
-    $con->query($sql4);
-    echo "Successfuly updated grant: " . $_SESSION['current_grant'];
-} else {
-    echo "Error updating record: " . $con->error;
 }
 
 $con->close();
