f4d1c3a33c5d029e7d4a695f14e7e7bccf81d83b
Zachary McGee <zachary.mcgee@siu.edu>
2019-02-24 03:06:11 -0600

    Update index.php

--- Grant Management Dashboard/index.php
+++ Grant Management Dashboard/index.php
@@ -1,5 +1,31 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>SIU - Grant Management System</title>
+  <meta name="description" content="Grant Management System for Southern Illinois University">
+  <meta name="author" content="Zachary McGee/Andrew Duckworth/Andrew Scott/Michael Olson">
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="css/login.css">
+
+</head>
+
+<body>
+  <div class="header">
+    <a href="index.html"><img src="images\siu-logo-horizontal.png"></a>
+  </div>
+
+<div class="content">
 <form action="auth.php" method="post">
   <input type="text" name="username" placeholder="Username">
   <input type="password" name="password" placeholder="Password">
               <button type="submit" id="login-button">Login</button>
       </form>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js" type="text/javascript"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+</body>
+</html>
