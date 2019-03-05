1dad32808261f10d61426c60c3abad43d8069f3d
Zachary McGee <zachary.mcgee@siu.edu>
2019-03-05 06:42:24 -0600

    Update index.php

--- Grant Management Dashboard/index.php
+++ Grant Management Dashboard/index.php
@@ -13,19 +13,29 @@
 </head>
 
 <body>
-  <div class="header">
-    <a href="index.html"><img src="images\siu-logo-horizontal.png"></a>
-  </div>
 
 <div class="content">
-<form action="auth.php" method="post">
-  <input type="text" name="username" placeholder="Username">
-  <input type="password" name="password" placeholder="Password">
+  <div class="login-right">
+      <form action="auth.php" method="post">
+        <a href="index.html"><img src="images\logo.png" style="width:100%;"></a>
+        <h1>Login or Sign Up</h1>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" placeholder="Enter Your Username....">
+        <label for="username">Password:</label>
+        <input type="password" id="password" name="password" placeholder="********">
               <button type="submit" id="login-button">Login</button>
       </form>
+    </div>
+  <div class="login-left">
+    <div class="left-description">
+
+    </div>
+  </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<script src="js/login.js"></script>
 </body>
 </html>
