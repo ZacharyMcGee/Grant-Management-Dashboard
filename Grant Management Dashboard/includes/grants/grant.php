c692225e80cfb5b93038404d4c22c285877b2166
Zachary McGee <zachary.mcgee@siu.edu>
2019-03-05 13:22:58 -0600

    Update grant.php

--- Grant Management Dashboard/includes/grants/grant.php
+++ Grant Management Dashboard/includes/grants/grant.php
@@ -40,15 +40,28 @@ $result = $con->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
+
     $row = $result->fetch_assoc();
+		$json = $row["jsondata"];
 		$myObj2 = new \stdClass();
 		$myObj2->x = $row["creation_date"];
-		$myObj2->y = $row["jsondata"];
+		$myObj2->y = $json;
 
 		array_push($a, $myObj2);
     $name = $row["name"];
 		$myJSON = json_encode($a);
+
+		if(is_numeric($row["dc_award"])){
+			 $formattedDCAward = '$' . number_format($row["dc_award"], 2);
+		}
+		else
+		{
+			$formattedDCAward = $row["dc_award"];
+		}
+
 		$timeChart = "<canvas id='timeChart'></canvas><script>linearTimeChart(" . $myJSON . ");</script>";
+		$dcRemaining = "<canvas id='dcLeftPieChart'></canvas><script>moneyLeftPieChart('dcLeftPieChart','" . $row["dc_award"] . "','" . $json . "');</script>";
+		$dcSpendingBreakdown = "<div class='dc-breakdown'>Award: <span class='awarded'>" . $formattedDCAward . "</span>\nSpent: <span class='spent' id='spent'></span><hr class='custom-hr'><span class='remaining' id='remaining'></span></div><script>setBreakdown('" . $json . "','" . $row["dc_award"] . "');</script>";
 }
 else
 {
@@ -96,34 +109,78 @@ $("#update-grant-data").click(function(){
 });
 </script>
 </head>
-<div class='full-card'>
+<div class="fourth-card">
+	<div class='card-title'>
+		<div class='card-title-text'><span class='parent-link'>Remaining Direct Cost</span></div>
+	</div>
+	<div class="remaining-awards">
+		<div class="dc-award-chart">
+			<?php echo $dcRemaining ?>
+		</div>
+		<div class="spending-breakdown">
+			<?php echo $dcSpendingBreakdown ?>
+		</div>
+	</div>
+</div>
+
+<div class="fourth-card">
+	<div class='card-title'>
+		<div class='card-title-text'><span class='parent-link'>Remaining Indirect Cost</span></div>
+	</div>
+	<div class="remaining-awards">
+		<div class="dc-award-chart">
+
+		</div>
+	</div>
+</div>
+
+<div class="fourth-card">
+	<div class='card-title'>
+		<div class='card-title-text'><span class='parent-link'>Something Else</span></div>
+	</div>
+	<div class="remaining-awards">
+		<div class="dc-award-chart">
+
+		</div>
+	</div>
+</div>
+
+<div class="fourth-card">
+	<div class='card-title'>
+		<div class='card-title-text'><span class='parent-link'>Something Else</span></div>
+	</div>
+	<div class="remaining-awards">
+		<div class="dc-award-chart">
+
+		</div>
+	</div>
+</div>
+<div class='full-card' style="margin-top:160px;">
   <div class='card-title'>
-    <div class='card-title-text'><span class='parent-link'><?php echo $name ?></span></div>
+    <div class='card-title-text'><span class='parent-link'>Spending Timeline</span></div>
   </div>
   <div class='body'>
-		<div class="remaining-awards">
-			<p>Remaining Direct Cost</p>
-		</div>
+
 		<div class="timechart">
 			<?php echo $timeChart ?>
 		</div>
+		</div>
 		<div class="update-grant-data">
-		<div class="drag-and-drop-description">
-			<p id="upload-excel-p">Update Grant Data</p><span id="small-hint" class="small-hint">(.xlsx format)</span>
-		</div>
-		<div id="drag-and-drop" class="drag-and-drop">
-			<div class="drag-and-drop-text">
-				<p>Drag and Drop File Here</p>
-			</div>
-			<div class="drag-and-drop-text-or">
-				<p>or</p>
-			</div>
-			<label for="file-upload" class="custom-file-upload">
-					Select File
-			</label>
-			<input id="file-upload" type="file"/>
-		</div>
-		<button id="update-grant-data" class="save-button" type="button"><i class="far fa-save" style="padding-right:10px;"></i>Update Grant</button>
-  </div>
+<div class="drag-and-drop-description">
+	<p id="upload-excel-p">Update Grant Data</p><span id="small-hint" class="small-hint">(.xlsx format)</span>
+</div>
+<div id="drag-and-drop" class="drag-and-drop">
+	<div class="drag-and-drop-text">
+		<p>Drag and Drop File Here</p>
+	</div>
+	<div class="drag-and-drop-text-or">
+		<p>or</p>
+	</div>
+	<label for="file-upload" class="custom-file-upload">
+			Select File
+	</label>
+	<input id="file-upload" type="file"/>
+</div>
+<button id="update-grant-data" class="save-button" type="button"><i class="far fa-save" style="padding-right:10px;"></i>Update Grant</button>
 </div>
 </div>
