3a019c223108fc6ea1bd91fe9acd59524e099622
Zachary McGee <zachary.mcgee@siu.edu>
2019-03-13 15:12:29 -0500

    Update Grant Data and Keep an Archive of the Old Record

--- Grant Management Dashboard/functions/update-grant.php
+++ Grant Management Dashboard/functions/update-grant.php
@@ -25,15 +25,14 @@ if ($result->num_rows > 0) {
   $old_date = $row['creation_date'];
 
   // Second query will take the current grant info and create an archive
-  $sql_createarchive = "INSERT INTO grant_archive (grantid, jsondata, creation_date)
-  VALUES ($currentGrant, '$old_json', '$old_date')";
+  $sql_createarchive = "INSERT INTO grant_archive (grantid, jsondata, creation_date, archive_date)
+  VALUES ($currentGrant, '$old_json', '$old_date', now())";
 
   if ($con->query($sql_createarchive) === TRUE) {
     // Get the new data and insert into the current grant
-    $sql_updatecurrentdata = "UPDATE grants SET jsondata='$jsondata' WHERE id=" . $_SESSION['current_grant'];
-    $sql_updatecreationdate = "UPDATE grants SET creation_date=now() WHERE id=" . $_SESSION['current_grant'];
+		$sql_updatecurrentdata = "UPDATE grants SET jsondata='$jsondata', last_update=now() WHERE id=" . $_SESSION['current_grant'];
 
-    if ($con->query($sql_updatecurrentdata) === TRUE && $con->query($sql_updatecreationdate) === TRUE)
+    if ($con->query($sql_updatecurrentdata) === TRUE)
     {
         echo "Successfuly updated grant!";
     }
@@ -42,7 +41,7 @@ if ($result->num_rows > 0) {
         echo "Error updating record: " . $con->error;
     }
   }
-  else 
+  else
   {
       echo "Error updating record: " . $con->error;
   }
