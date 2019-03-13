b13efe0895b07950242a901f5055a5075e2b5ac8
Zachary McGee <zachary.mcgee@siu.edu>
2019-03-12 21:27:14 -0500

    Add Favicon

--- Grant Management Dashboard/dashboard.php
+++ Grant Management Dashboard/dashboard.php
@@ -33,6 +33,7 @@ if ($result->num_rows > 0) {
   <title>Grant Management Dashboard</title>
   <meta name="description" content="Grand Management Dashboard">
   <meta name="author" content="AMAZING">
+	<link rel='shortcut icon' type='image/x-icon' href='images/favicon.ico' />
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 </head>
