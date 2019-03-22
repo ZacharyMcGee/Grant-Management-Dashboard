ace6dad2c0b4c0ada102531dfaed3b68b2b994d4
AndrewScott97 <45775674+AndrewScott97@users.noreply.github.com>
2019-03-22 18:12:51 -0500

    Revert "Merge branch 'master' of https://github.com/ZacharyMcGee/Grant-Management-Dashboard"
    This reverts commit 3f778328b3ad53ead851d9e00f2292a1e89db48d, reversing
    changes made to 6466f0fe3da9dce26a9aa5ffaec3dc544ac3b8f5.

--- Grant Management Dashboard/functions/delete-grant.php
+++ Grant Management Dashboard/functions/delete-grant.php
@@ -8,14 +8,31 @@ if ( mysqli_connect_errno() ) {
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-$grantid = json_encode($_GET['id']);
+// Get current user
+$userId = $_SESSION['id'];
+// Get current grant
+$currentGrant = $_SESSION['current_grant'];
+// Get POST values
+$jsondata = json_encode($_POST['jsondata']);
 
-$sql = "DELETE FROM grants WHERE id=" . $grantid;
+$id = $_POST['id'];
 
-if ($con->query($sql) === TRUE) {
-    echo "Successfuly deleted grant!";
-} else {
-    echo "Error updating record: " . $con->error;
+if($id > 0){
+    
+    $check = mysqli_query($con, "SELECT * FROM posts WHERE id=" . $id);
+    $totalRows = mysqli_num_rows($check);
+    
+    if($totalRows > 0){
+        $query = "DELETE FROM grants WHERE id=" . $id;
+        mysqli_query($con, $query);
+        echo 1;
+        exit;
+    }
+    
 }
+
+
+
+
 
 ?>
